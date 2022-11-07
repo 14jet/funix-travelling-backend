6c7cdba4be1db432a212b9e2089ceb5e48a26ae1
ntav <ntav2095@gmail.com>
2022-11-07 15:19:50 +0700

    x

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,11 +1,8 @@
 const cors = require("cors");
-// const config = require("config");
+const config = require("config");
 
 const corsOption = {
-  origin: [
-    "http://localhost:3000",
-    "https://travelling-website-funix-v1.web.app/",
-  ],
+  origin: config.get("cors.whiteList"),
   credentials: true,
 };
 
