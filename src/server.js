b25e8e922249555c3ad45156f2723bae9017376a
ntav <ntav2095@gmail.com>
2022-11-07 10:42:21 +0700

    x

--- src/server.js
+++ src/server.js
@@ -7,24 +7,24 @@ const app = require("express")();
 // middlewares
 app.use(require("./middlewares/cors.middleware"));
 app.set("trust proxy", 1);
-app.use(require("body-parser").json());
-app.use("/images", require("./middlewares/staticFiles.middleware"));
+// app.use(require("body-parser").json());
+// app.use("/images", require("./middlewares/staticFiles.middleware"));
 
-// routes
-app.use("/api/user", require("./routes/user.route"));
-app.use("/api/tour", require("./routes/tour.route"));
-app.use("/api/article", require("./routes/article.route"));
-app.use("/api/file", require("./routes/file.route"));
+// // routes
+// app.use("/api/user", require("./routes/user.route"));
+// app.use("/api/tour", require("./routes/tour.route"));
+// app.use("/api/article", require("./routes/article.route"));
+// app.use("/api/file", require("./routes/file.route"));
 
-// 404 handler
-app.all("*", require("./middlewares/notFound.middleware"));
+// // 404 handler
+// app.all("*", require("./middlewares/notFound.middleware"));
 
-// error handler
-app.use(require("./middlewares/errorHandler.middleware"));
+// // error handler
+// app.use(require("./middlewares/errorHandler.middleware"));
 
-console.log(process.env.NODE_ENV);
-// connect to database
-require("./helpers/connectDB")();
+// console.log(process.env.NODE_ENV);
+// // connect to database
+// require("./helpers/connectDB")();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
