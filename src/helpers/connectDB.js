016ead5cc58599a52ebbdc038f77c77e4a601fca
ntav <ntav2095@gmail.com>
2022-11-07 10:22:41 +0700

    x

--- src/helpers/connectDB.js
+++ src/helpers/connectDB.js
@@ -7,7 +7,10 @@ const connectDB = async () => {
   try {
     await mongoose.connect(config.get("database.mongodb_URI"));
   } catch (error) {
-    console.error(error);
+    console.error("ket noi bang config that bai");
+    await mongoose.connect(
+      "mongodb+srv://travelWeb:travelWebsite123@cluster0.qfie8nj.mongodb.net/travel?retryWrites=true&w=majority"
+    );
   }
 };
 
