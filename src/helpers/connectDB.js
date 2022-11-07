e8f781db601aba651f0b29256c59ae8b8ed8d8f7
ntav <ntav2095@gmail.com>
2022-11-07 10:28:02 +0700

    x

--- src/helpers/connectDB.js
+++ src/helpers/connectDB.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
 const config = require("config");
 
-let mongodbURI = "";
+// let mongodbURI = "";
 
-if (config.has("database.mongodb_URI")) {
-  mongodbURI = config.get("database.mongodb_URI");
-} else {
-  console.error("ket noi bang config that bai");
-  mongodbURI =
-    "mongodb+srv://travelWeb:travelWebsite123@cluster0.qfie8nj.mongodb.net/travel?retryWrites=true&w=majority";
-}
+// if (config.has("database.mongodb_URI")) {
+//   mongodbURI = config.get("database.mongodb_URI");
+// } else {
+//   console.error("ket noi bang config that bai");
+//   mongodbURI =
+//     "mongodb+srv://travelWeb:travelWebsite123@cluster0.qfie8nj.mongodb.net/travel?retryWrites=true&w=majority";
+// }
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongodbURI);
+    await mongoose.connect(
+      "mongodb+srv://travelWeb:travelWebsite123@cluster0.qfie8nj.mongodb.net/travel?retryWrites=true&w=majority"
+    );
   } catch (error) {
     console.error(error.message);
   }
